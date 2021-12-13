5199a6b9b48a0cce7fcc77ede2fd6d36ebcfb25b
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-13 15:05:16 +0530

    Update __main__.py

--- lycia/__main__.py
+++ lycia/__main__.py
@@ -1,6 +1,12 @@
-from lycia import LYCIA
 from lycia.Lycia import start, lycia, lyciainline
 from pyrogram import filters
+from lycia import API_ID , API_HASH , TOKEN
+
+LYCIA = Client(
+        ':memory:', 
+         api_id=API_ID, 
+         api_hash=API_HASH,
+         bot_token=TOKEN)
 
 if __name__ == "__main__":
     LYCIA.run()
